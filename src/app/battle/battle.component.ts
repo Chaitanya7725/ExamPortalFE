7b754870c82b1a8524a84800c8c7e4a492ce7517
Chaitanya Takalikar <chaitanya7725@gmail.com>
2021-07-01 14:30:08 +0530

    Q&A Stats count rectified,+!answered count,+Scroll

--- src/app/battle/battle.component.ts
+++ src/app/battle/battle.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
-import { CountdownComponent, CountdownConfig } from 'ngx-countdown';
+import { CountdownComponent } from 'ngx-countdown';
 import { BattleService } from '../battle.service';
 import { Question } from '../question';
 import { QuizService } from '../quiz.service';
@@ -34,18 +34,8 @@ export class BattleComponent implements OnInit {
     os:0,
     quizId:0
   };
-  // p=  {
-  //   id:0,
-  //   question:"",
-  //   mark:0,
-  //   quiz:{},
-  //   option1:"",
-  //   option2:"",
-  //   option3:"",
-  //   option4:"",
-  //   correctOption:0
-  // };
   quizDetails:any;
+  quizDetailsStatus:any;
   time:number=0;
   question:Question=new Question();
   optionSelected: number=0;
@@ -53,6 +43,7 @@ export class BattleComponent implements OnInit {
   battleImage: any;
   base64Data: any;
   nextIndex: any;
+  isCalled: boolean=false;
   constructor(private service:BattleService,
     private route: ActivatedRoute,
     private quiz:QuizService,
@@ -74,10 +65,6 @@ export class BattleComponent implements OnInit {
         this.config={ leftTime:(this.quizDetails.time)*60};
     });
   }
-  // ngAfterContentInit(){
-  //   console.log("ngAfterContentInit")
-  //   this.setLeftTime();
-  // }
 
   toggle() {
     this.show = !this.show;
@@ -97,10 +84,20 @@ export class BattleComponent implements OnInit {
         this.numbers=params
       });
   }
+  
+  passCurrentQuestionId(i:number){
+    this.questionId=i;
+    // if((this.numbers.length-1)== this.numbers.indexOf(i) ){
+    //   this.isDisabled =true;
+    // }else{
+    //   this.isDisabled=false;
+    // }
+    this.getQuestionById(i);
+  }
 
   getQuestionById(optionalParams:number){
     this.retrievedImage="";
-    this.clear();
+    this.clearAfterClickingIndex();
     this.service.getQuestionById(optionalParams).subscribe(
       (params)=>{
         this.populateQuestionData=params;
@@ -116,33 +113,36 @@ export class BattleComponent implements OnInit {
         }
       }
     );
-  }
-
-  passCurrentQuestionId(i:number){
-    this.questionId=i;
-    // if((this.numbers.length-1)== this.numbers.indexOf(i) ){
-    //   this.isDisabled =true;
-    // }else{
-    //   this.isDisabled=false;
-    // }
-    this.getQuestionById(i);
+    this.getQuizDetails();
   }
 
   saveAnswer(){
     this.options.cqid=this.questionId;
-    if(this.optionSelected==0){
-      this.options.os=0;
-    } else if(this.optionSelected>0){
-      this.options.os=this.optionSelected;
-    } else{
-      this.options.os=this.populateQuestionData.selectedOption;
+    if(this.populateQuestionData.selectedOption>0 && this.isCalled){
+      console.log("If");
+      if(this.isCalled){
+        console.log("If If "+this.isCalled);
+        this.options.os=this.optionSelected;
+      }else{
+        console.log("If else "+this.isCalled);
+        this.options.os=this.populateQuestionData.selectedOption;
+      }
+    }else{
+      console.log("Else");
+      if(this.isCalled){
+        console.log("Else if "+this.isCalled);
+        this.options.os=this.optionSelected;
+      }else{
+        console.log("Else else "+this.isCalled);
+        this.options.os=this.populateQuestionData.selectedOption;
+      }
     }
     // this.options.os=this.optionSelected || this.populateQuestionData.selectedOption;
     this.options.quizId=this.quizIdV;
-    console.log(this.populateQuestionData.selectedOption+" "+this.optionSelected);
     // if(this.options.os !=NaN && this.populateQuestionData.selectedOption!=this.options.os){
     if(this.options.os!=NaN && this.options.os!=undefined){
-      console.log("calling api ");
+      console.log("SelectedOption "+this.populateQuestionData.selectedOption);
+      console.log("calling api "+JSON.stringify(this.options));
       this.service.saveAnswer(this.options).subscribe();
     }
 
@@ -152,18 +152,25 @@ export class BattleComponent implements OnInit {
     }else if(currentIndex<this.numbers.length){
       this.nextIndex=currentIndex+1;
     }
+    console.log(this.nextIndex);
     this.passCurrentQuestionId(this.numbers[this.nextIndex]);
     this.clear();
+    this.isCalled=false;
+    // console.log("--------------------------------------------")
   }
 
   optionSel(event:any){
-    // console.log(event.target.value);
+    this.isCalled=true;
     this.optionSelected=event.target.value;
+  }
+  clearAfterClickingIndex(){
+    this.clearResponse=false;
   }
   clear(){
     this.clearResponse=false;
-    this.options.os=0;
-    this.populateQuestionData.selectedOption="";
+    this.isCalled=true;
+    this.optionSelected=0;
+    this.populateQuestionData.selectedOption=5;
   }
 
   handleEvent(event:any){
@@ -173,13 +180,17 @@ export class BattleComponent implements OnInit {
     }
   }
 
-  // getQuizDetails(){
-  //   this.quiz.getQuizById(this.quizIdV).subscribe(
-  //     params=> {
-  //       this.quizDetails=params;
-  //       this.quizDetails=JSON.parse(this.quizDetails);
-  //   });
-  // }
+  getQuizDetails(){
+    this.quiz.getQuizById(this.quizIdV).subscribe(
+      params=> {
+        this.quizDetailsStatus=params;
+        this.quizDetailsStatus=JSON.parse(this.quizDetailsStatus);
+        this.quizDetails.answered=this.quizDetailsStatus.answered;
+        this.quizDetails.notAnswered=this.quizDetailsStatus.notAnswered;
+
+        console.log("2 details "+JSON.stringify(this.quizDetailsStatus));
+    });
+  }
 
   // setLeftTime(){
   //   this.config={ leftTime: (this.quizDetails.time)*60 };
